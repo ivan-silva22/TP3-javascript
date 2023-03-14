a4e5dd772ee69ab50e02a8cd30d448c9b6c5d52b
Ivan Silva <silvaivan280@gmail.com>
2023-03-14 18:06:33 -0300

    solucion del ejercicio 1

---
+++ js/ejercicio-1.js
@@ -0,0 +1,6 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+meses.forEach(mes => document.write(`<ul><li>${mes}</li></ul>`));
+
+
+
