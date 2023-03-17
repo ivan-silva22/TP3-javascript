a58582703250cc86d80f103531484435dc2edb19
Ivan Silva <silvaivan280@gmail.com>
2023-03-16 22:43:12 -0300

    solucion del ejercicio 2

---
+++ js/ejercicio-2.js
@@ -0,0 +1,27 @@
+let ciudades = [];
+
+do {
+   let ciudad = prompt("Ingrese el nombre de una Ciudad");
+   ciudades.push(ciudad);
+} while (confirm('¿Desea seguir ingresando Ciudades?'));
+
+ciudades.push("paris");
+document.write(`<h2>${ciudades}</h2>`); 
+document.write(`<p>Loguitud del array: ${ciudades.length}</p>`);
+document.write(`<p>Primera posición: ${ciudades[0]}, tercera posición: ${ciudades[2]}, Ultima posición: ${ciudades[ciudades.length - 1]}</p>`);
+document.write(`<p>Elemento que ocupa la segunda posición: ${ciudades[1]}</p>`)
+ciudades[1] = "barcelona";
+document.write(`<h2>${ciudades}</h2>`); 
+
+
+
+
+
+
+
+
+
+
+
+
+
