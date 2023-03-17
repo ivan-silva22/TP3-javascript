db2df3b22eacf490c3c841a6a6d825ac113311b0
Ivan Silva <silvaivan280@gmail.com>
2023-03-16 23:06:04 -0300

    correccion de length

--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -5,10 +5,10 @@ do {
    ciudades.push(ciudad);
 } while (confirm('¿Desea seguir ingresando Ciudades?'));
 
-document.write(`<h2>${ciudades}</h2>`); 
-ciudades.push("paris");
-document.write(`<p>Loguitud del array: ${ciudades.length}</p>`);
+document.write(`<h2>${ciudades}</h2>`);
+document.write(`<p>Longitud del array: ${ciudades.length}</p>`); 
 document.write(`<p>Primera posición: ${ciudades[0]}, tercera posición: ${ciudades[2]}, Ultima posición: ${ciudades[ciudades.length - 1]}</p>`);
+ciudades.push("paris");
 document.write(`<p>Elemento que ocupa la segunda posición: ${ciudades[1]}</p>`)
 ciudades[1] = "barcelona";
 document.write(`<h2>${ciudades}</h2>`); 
