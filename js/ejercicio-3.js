ef6548385dda91b6002c254c2a0ab1a8e2415363
Ivan Silva <silvaivan280@gmail.com>
2023-03-19 12:19:30 -0300

    solucion del ejercicio 3

--- js/ejercicio-3.js
+++ js/ejercicio-3.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-let numeroDeApariciones = [];
-=======
 let numeros = [];
 let numeroDeApariciones = []
 let elementosUnicos = []
 let contador = 1;
->>>>>>> ejercicio3
 
 for (i = 1; i <= 10; i++){
     let primerDado = Math.floor(Math.random() * (5 + 1) + 1);
@@ -13,11 +9,6 @@ for (i = 1; i <= 10; i++){
     document.write(`<h3>Lanzamiento ${i} del primer dado: ${primerDado}</h3>`);
     document.write(`<h3>Lanzamiento ${i} del segundo dado: ${segundoDado}</h3>`);
     suma = primerDado + segundoDado;
-<<<<<<< HEAD
-    document.write("suma " + suma);
-    
-   
-=======
     numeros.push(suma);
 }
 
@@ -39,14 +30,11 @@ document.write(`<h4>Suma ${numeros}</h4>`);
 
 for (j = 0;j < elementosUnicos.length; j++){
     document.write(`<h4>El valor ${elementosUnicos[j]} se repite ${numeroDeApariciones[j]}</h4>`);
->>>>>>> ejercicio3
 }
 
 
 
 
-<<<<<<< HEAD
-=======
 
 
 
@@ -57,7 +45,6 @@ for (j = 0;j < elementosUnicos.length; j++){
 
 
 
->>>>>>> ejercicio3
     
 
 
